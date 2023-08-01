155ff521525ecae17bc0642ffec1ddce6916908c
Kareem Dana <kdana@wtamu.edu>
2023-08-01 15:51:05 -0500

    Added app icon

--- app/src/main/java/com/example/reminders/MainActivity.kt
+++ app/src/main/java/com/example/reminders/MainActivity.kt
@@ -20,10 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.reminders.data.DataSource
 import com.example.reminders.model.Reminder
 import com.example.reminders.ui.theme.RemindersTheme
